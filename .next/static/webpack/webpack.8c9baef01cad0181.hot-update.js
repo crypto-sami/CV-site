7a213be5b0cc9a99b102d9f8575229024da4c3e9
samiturk8 <samiturk8@gmail.com>
2022-06-15 13:47:05 +0100

    re-done some code

--- .next/static/webpack/webpack.8c9baef01cad0181.hot-update.js
+++ .next/static/webpack/webpack.8c9baef01cad0181.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c6081538e17b5e57"; }
+/******/ 	__webpack_require__.h = function() { return "a6cbe1f296a122a5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
